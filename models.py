3aaba462df55d14447484d45b3b5ff4cccbe425d
Diego Hernández <diegoehg@gmail.com>
2021-03-10 21:06:52 -0600

    Feat: Adds setup_db function
    This function sets up a connection to the database from a environment variable

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
 
+os_database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=os_database_path):
+    """ setup_db(app) binds a flask application and a SQLAlchemy service """
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
