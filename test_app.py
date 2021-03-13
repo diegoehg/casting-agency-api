dc5903415127355a5db7c15372a9bd4615c1e9cd
Diego Hernández <diegoehg@gmail.com>
2021-03-13 15:59:35 -0600

    Test: Adds fixture for getting test_client
    The set up of testing_client is encapsulated in a fixture.

--- test_app.py
+++ test_app.py
@@ -1,22 +1,29 @@
 from app import create_app
-from models import Movie
-
-database_dialect = "postgresql"
-database_user = "casting_tester"
-database_password = "testinguser"
-database_location = "localhost:5432"
-database_name = "casting_agency_test"
-
-test_config = {
-    'DATABASE_URL': f"{database_dialect}://{database_user}:{database_password}@{database_location}/{database_name}"
-}
-
-app = create_app(test_config)
-client = app.test_client
+from models import db, Movie
+import pytest
 
 
-def test_get_movies_default_page():
-    response = client().get('/movies')
+@pytest.fixture(scope='module')
+def client():
+    database_dialect = "postgresql"
+    database_user = "casting_tester"
+    database_password = "testinguser"
+    database_location = "localhost:5432"
+    database_name = "casting_agency_test"
+
+    test_config = {
+        'DATABASE_URL': f"{database_dialect}://{database_user}:{database_password}@{database_location}/{database_name}"
+    }
+
+    app = create_app(test_config)
+    with app.test_client() as testing_client:
+        with app.app_context():
+            db.create_all()
+            yield testing_client
+
+
+def test_get_movies_default_page(client):
+    response = client.get('/movies')
     assert response.status_code == 200
 
     movies = [m.format() for m in Movie.query.order_by(Movie.id).slice(0, 10)]
@@ -26,3 +33,7 @@ def test_get_movies_default_page():
     assert data['success']
     assert data['movies'] == movies
     assert data['total_movies'] == total_movies
+
+
+if __name__ == "__main__":
+    pytest.main()
