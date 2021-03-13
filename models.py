a746a45b768e95378e90ba65f52923a3c8442f98
Diego Hernández <diegoehg@gmail.com>
2021-03-13 14:29:38 -0600

    Feat: Sets up database migrations
    Incorporates Flask-Migrate and a script for running it.

--- models.py
+++ models.py
@@ -13,7 +13,6 @@ def setup_db(app, database_path=os_database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
 
 
 class Movie(db.Model):
