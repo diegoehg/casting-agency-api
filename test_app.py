8bcda4e9f16f633e4bbb1b28a79192a4fd972311
Diego Hernández <diegoehg@gmail.com>
2021-03-17 21:16:57 -0600

    Test: Added fixtures that contain bearer tokens
    The GET /movies endpoint is tested against different role tokens.

--- test_app.py
+++ test_app.py
@@ -24,11 +24,25 @@ def client():
             yield testing_client
 
 
+def get_authorization_header(token):
+    return {
+        'Authorization': f'Bearer {token}'
+    }
+
+
 @pytest.fixture(scope='module')
 def token_casting_assistant():
-    yield {
-        "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IllKTHRSdUJFM1QxVXVNR0VkTWdaeCJ9.eyJpc3MiOiJodHRwczovL2Rldi1iYWxpYW4udXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDYwMGRmNmI2ZmZjYmUyMDA2YTg4NmUyZCIsImF1ZCI6ImNhc3RpbmctYWdlbmN5LWF1dGgtYXBpIiwiaWF0IjoxNjE2MDMzNzIzLCJleHAiOjE2MTYwNDA5MjMsImF6cCI6Ik9yQTZkRFZiVmVYZ01YQkFsVHFuWHI4UE9RU2MyYVY4Iiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJnZXQ6YWN0b3JzIiwiZ2V0Om1vdmllcyJdfQ.chIxG2SEouVdBMKNS6WeS4f3o6R_OVuFqjbPnog7soan07mX2x753gcefEqRryx1WX3uXU6fQd3B5aya4iMFmSWmIMXpAeSVZ8rsm9fCOR1oRrye6778xXZlH1o4RVUvAAU-sLKbVuvtaHVGWhrONYVxxnNVBLzenJf6gR2iArUUfloG2cUJ4aykj5g609LasHHXWM8kJxQFa7KNuDNCyrWe7CsdR-8RDXqyO643NgpF8okbP7ygr7z-01Hv16E-hnsAh1CzzBHvaoFF8zOahGhSQQ_9vNkdAl0-u39DHpFPdNakZDG45vZ5UZoGZdP3_wOLIr2lDOwf-cYIY-evKQ"
-    }
+    yield get_authorization_header("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IllKTHRSdUJFM1QxVXVNR0VkTWdaeCJ9.eyJpc3MiOiJodHRwczovL2Rldi1iYWxpYW4udXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDYwMGRmNmI2ZmZjYmUyMDA2YTg4NmUyZCIsImF1ZCI6ImNhc3RpbmctYWdlbmN5LWF1dGgtYXBpIiwiaWF0IjoxNjE2MDMzNzIzLCJleHAiOjE2MTYwNDA5MjMsImF6cCI6Ik9yQTZkRFZiVmVYZ01YQkFsVHFuWHI4UE9RU2MyYVY4Iiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJnZXQ6YWN0b3JzIiwiZ2V0Om1vdmllcyJdfQ.chIxG2SEouVdBMKNS6WeS4f3o6R_OVuFqjbPnog7soan07mX2x753gcefEqRryx1WX3uXU6fQd3B5aya4iMFmSWmIMXpAeSVZ8rsm9fCOR1oRrye6778xXZlH1o4RVUvAAU-sLKbVuvtaHVGWhrONYVxxnNVBLzenJf6gR2iArUUfloG2cUJ4aykj5g609LasHHXWM8kJxQFa7KNuDNCyrWe7CsdR-8RDXqyO643NgpF8okbP7ygr7z-01Hv16E-hnsAh1CzzBHvaoFF8zOahGhSQQ_9vNkdAl0-u39DHpFPdNakZDG45vZ5UZoGZdP3_wOLIr2lDOwf-cYIY-evKQ")
+
+
+@pytest.fixture(scope='module')
+def token_casting_director():
+    yield get_authorization_header("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IllKTHRSdUJFM1QxVXVNR0VkTWdaeCJ9.eyJpc3MiOiJodHRwczovL2Rldi1iYWxpYW4udXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDYwNTJiMmQ1ODQzNWRhMDA2ODg0ODI2OSIsImF1ZCI6ImNhc3RpbmctYWdlbmN5LWF1dGgtYXBpIiwiaWF0IjoxNjE2MDM2Mzk2LCJleHAiOjE2MTYwNDM1OTYsImF6cCI6Ik9yQTZkRFZiVmVYZ01YQkFsVHFuWHI4UE9RU2MyYVY4Iiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGU6YWN0b3JzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInVwZGF0ZTphY3RvcnMiLCJ1cGRhdGU6bW92aWVzIl19.sPGA9B1C-8K51FfeSl48Guwf7eRGEjCxSl_LUBiuBw_RlA33Ls346VWTmUg0NmVR71RVqHcWS0HbjnISFUr9LHWcTbSXRn-5IitrYp2CcHhGvSJpD_UH1zAOZGRWrRxo63-gI_7ws1f3qbXCgqdc0HWdCvcZpDYvEoYFTZfy8yVdPtbWRqZQhUenL9sAKEJmCRLALH_OGO6SWBB5OyZcyyDswJT26hQjIaBG5Y-ECuSf8pPrOMRZI9GfPCKGoGNmk93EixOOz44uS1hcLY4tzLJY1SR-Seuw5sXdhFLZYjGdmXGeigGLkq3fcMXahMjzdeWB_exJY5tfOm4iRgFlGQ")
+
+
+@pytest.fixture(scope='module')
+def token_executive_producer():
+    yield get_authorization_header("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IllKTHRSdUJFM1QxVXVNR0VkTWdaeCJ9.eyJpc3MiOiJodHRwczovL2Rldi1iYWxpYW4udXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDYwNTJiNDY2MjYyNjFhMDA2ZmE5MGJhOSIsImF1ZCI6ImNhc3RpbmctYWdlbmN5LWF1dGgtYXBpIiwiaWF0IjoxNjE2MDM2NzY2LCJleHAiOjE2MTYwNDM5NjYsImF6cCI6Ik9yQTZkRFZiVmVYZ01YQkFsVHFuWHI4UE9RU2MyYVY4Iiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGU6YWN0b3JzIiwiZGVsZXRlOm1vdmllcyIsImdldDphY3RvcnMiLCJnZXQ6bW92aWVzIiwicG9zdDphY3RvcnMiLCJwb3N0Om1vdmllcyIsInVwZGF0ZTphY3RvcnMiLCJ1cGRhdGU6bW92aWVzIl19.ESjKGwdKL2idPjdPFlYDIKBAxrBsqlji52aNOprVsojxYOcg5wbJjmcBOJhytTA8qdcWSZonQNU2SNBvu2ahYWbf2EPVr8OC0nFHph9QLmXvDQeccwybjxuwyytoP5koaHdVbv4sE7kXoJCNaBU9-kOCyv5w7E2QxR00kIDdXohECMQiv1eQfcTnJXC5pYuNJpJYgMogMYbrUozr057giAKmaONdjowSZviQYy24ju7jXEJebXRLgioy-I48F2vzmSszFg-wn84IAo6hKEiKqiUHjsHTfiCyHH3mNqXZuLf1thkVg1vZvbkat856w1mQC6sxCNDELU-PBl65yj2xMQ")
 
 
 def test_get_movies_default_page(client, token_casting_assistant):
@@ -72,6 +86,24 @@ def test_404_in_get_movies_unexistent_page(client, token_casting_assistant):
     assert not data['success']
     assert data['error'] == 404
     assert data['message'] == 'Resource not found'
+
+
+def test_get_movies_with_casting_assistant_token(client, token_casting_assistant):
+    response = client.get('/movies',
+                          headers=token_casting_assistant)
+    assert response.status_code == 200
+
+
+def test_get_movies_with_casting_director_token(client, token_casting_director):
+    response = client.get('/movies',
+                          headers=token_casting_director)
+    assert response.status_code == 200
+
+
+def test_get_movies_with_executive_producer_token(client, token_executive_producer):
+    response = client.get('/movies',
+                          headers=token_executive_producer)
+    assert response.status_code == 200
 
 
 def test_401_when_request_does_not_contain_authorization_header(client):
