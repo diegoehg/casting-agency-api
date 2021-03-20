146a71efa665adb9e7c0a4fd87d38235a35e2922
Diego Hernández <diegoehg@gmail.com>
2021-03-19 23:28:10 -0600

    Test: GET /actors/{id} with different roles.
    Tests if GET /actors/{id} works with the three different roles.

--- test_app.py
+++ test_app.py
@@ -238,6 +238,24 @@ def test_404_when_get_actor_unexistent_id(client, token_casting_assistant):
     assert data['message'] == 'Resource not found'
 
 
+def test_get_actor_with_casting_assistant(client, token_casting_assistant):
+    response = client.get('/actors/1',
+                          headers=token_casting_assistant)
+    assert response.status_code == 200
+
+
+def test_get_actor_with_casting_director(client, token_casting_director):
+    response = client.get('/actors/1',
+                          headers=token_casting_director)
+    assert response.status_code == 200
+
+
+def test_get_actor_with_executive_producer(client, token_executive_producer):
+    response = client.get('/actors/1',
+                          headers=token_executive_producer)
+    assert response.status_code == 200
+
+
 def test_401_when_request_does_not_contain_authorization_header(client):
     response = client.get('/movies')
     assert response.status_code == 401
